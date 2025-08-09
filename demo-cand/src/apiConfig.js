5c7884130aab98c8eb89524e17988b484eb2193e
Simon McCallum <simon.mccallum@vuw.ac.nz>
2025-08-09 16:16:50 +1200

    fixing url

--- demo-cand/src/apiConfig.js
+++ demo-cand/src/apiConfig.js
@@ -2,4 +2,4 @@ export const API_BASE =
   (window.__APP_CONFIG__ && window.__APP_CONFIG__.API_BASE) ||
   (window.location.hostname === "localhost"
     ? "http://localhost:5050"
-    : "https://ludogogy.ac.nz/webofinfluence/api");
+    : "https://ludogogy.co.nz/webofinfluence/api");
