2e9c27b63048fabec94262af967e003c44bcf223
kng-04 <nkrystal.2004@gmail.com>
2025-09-10 15:48:37 +1200

    Add React build configuration and built assets for login page deployment

--- deploy/src/main.jsx
+++ deploy/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 // pages
-import HomePage from './Homepage'; 
+import HomePage from './Hompage.jsx'; 
 //import CandidateOverview from './CandidateOverview.jsx';
 //import MeetingsSearch from './MeetingsSearch.jsx';
 //import PersonProfile from './PersonProfile.jsx';
 import LoginPage from './Loginpage.jsx'; 
-import Settings from './Settings.jsx';
+//import Settings from './Settings.jsx';
 
 // auth
 import AuthProvider from './auth/AuthProvider';
@@ -16,19 +16,19 @@ import ProtectedRoute from './auth/ProtectedRoute';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <Router basename="/WebOfInfluenceResearch">
+    <Router basename="/webofinfluence">
       <Routes>
-        {/* Public routes */}
+        {/* Default route - always shows login page first */}
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         
         {/* Protected routes - all pages require authentication */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/home" replace />} />
           <Route path="/home" element={<HomePage />} />
           {/*<Route path="/candidate-overview" element={<CandidateOverview />} />
           <Route path="/meetings" element={<MeetingsSearch />} />
-          <Route path="/person/:firstName/:lastName" element={<PersonProfile />} /> */}
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/person/:firstName/:lastName" element={<PersonProfile />} /> 
+          <Route path="/settings" element={<Settings />} /> */}
         </Route>
       </Routes>
     </Router>
