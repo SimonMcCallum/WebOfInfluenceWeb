861134110e42ad4b6bfc6db01216f42a9bc6fdab
kng-04 <nkrystal.2004@gmail.com>
2025-09-10 20:59:04 +1200

    fix to revert to working state

--- deploy/src/apiConfig.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE =
-  (window.__APP_CONFIG__ && window.__APP_CONFIG__.API_BASE) ||
-  (window.location.hostname === "localhost"
-    ? "http://localhost:5050"
-    : "/webofinfluence/api");
