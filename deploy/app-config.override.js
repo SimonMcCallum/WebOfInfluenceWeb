3fd16a14f4eacad3c7067486dfc98ade549aa569
kng-04 <nkrystal.2004@gmail.com>
2025-09-12 10:04:54 +1200

    restored files

--- deploy/app-config.override.js
+++ deploy/app-config.override.js
@@ -1,14 +1,7 @@
 /* Runtime config for production without .htaccess rewrites.
    Use index.php front controller with ?route=... to reach endpoints.
 */
-
-/*
-Old:
 window.__APP_CONFIG__ = {
   // Example full URL: https://ludogogy.co.nz/php-api/index.php
   API_BASE: '/php-api/index.php'
-};*/
-
-window.__APP_CONFIG__ = {
-  API_BASE: '/webofinfluence/api/index.php'
 };
