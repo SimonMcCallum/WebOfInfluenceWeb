a026af808faeab0d3feac6a4dc7f98a9558075f8
kng-04 <nkrystal.2004@gmail.com>
2025-09-11 21:19:45 +1200

    Add: config.php

---
+++ deploy/dist/api/config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+  'DB_HOST' => 'localhost',
+  'DB_USER' => 'ludog319_kng',
+  'DB_PASS' => 'WFoSE!',
+  'DB_NAME' => 'ludog319_webofinfluence',
+  'API_TOKEN' => 'Admin-489-project-WoI',
+  'API_PROTECT_ALL' => false,
+];
